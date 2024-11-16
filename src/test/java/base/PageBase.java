206b199fd48a82eeb79d03f7e3fe354a0f7c0ccb
murali ambekar <muralimambekar@gmail.com>
2024-11-16 23:45:45 +0530

    added method to takescreenshot

--- src/test/java/base/PageBase.java
+++ src/test/java/base/PageBase.java
@@ -1,10 +1,14 @@
 package base;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.codehaus.plexus.util.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
@@ -23,6 +27,20 @@ public class PageBase {
 	public void click(WebElement element) {
 		element.click();
 		System.out.println("clicked");
+	}
+	
+	public void takeScreenshot() {
+		String dateTime=String.valueOf(System.currentTimeMillis());
+		File scrFile= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(scrFile, new File("./screenshots/img_"+dateTime));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Screenshot captured successfully");
+		
 	}
 	
 	
