f88c0b169eb226983ea6b07686d38f5f62ccf1a2
murali ambekar <muralimambekar@gmail.com>
2024-11-23 19:43:09 +0530

    removed webdriver manager

--- src/test/java/base/testBase.java
+++ src/test/java/base/testBase.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import pageObjects.homePage;
 import utilities.DriverFactory;
 import utilities.configFile;
